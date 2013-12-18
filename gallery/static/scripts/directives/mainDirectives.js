034db6b542b64b220aa93e804507b573b197bca9
stackBundles <iain.j.mingo@gmail.com>
2013-12-18 12:11:26 -0500

    adding angularjs code to control transitions

--- gallery/static/scripts/directives/mainDirectives.js
+++ gallery/static/scripts/directives/mainDirectives.js
@@ -1,18 +1,21 @@
 var directives = angular.module('ScaffoldsApp.directives', []);
 
+// A stub directive is instantiated for each sidebar game. The linking function
+// calls the queryService with the appropriate game code
 directives.directive('stub', function(queryService) {
 	return {
 		restrict: 'E',
 		link: function(scope, elem, attrs) {
 
 			scope.loadgame = function(code) {
-				console.log(code);
 				queryService.getGame(code);
 			};
 		}
 	}
 })
 
+// teambox directives use an isolated scope to prevent them from changing each other's
+// variables.  The linking function controls the display logic.
 directives.directive('teambox', function() {
 	return {
 		restrict: 'E',
@@ -25,9 +28,17 @@ directives.directive('teambox', function() {
 			}
 
 			scope.team.switch = function(type) {
-				console.log('switch: ' + type);
 				scope.team.stats = type;
 			}
+
+			// a little CSS animation to keep things stylish
+			scope.$on('hide', function() {
+				elem.addClass('fadeOut');
+			})
+
+			scope.$on('show', function() {
+				elem.removeClass('fadeOut');
+			})
 		}
 	}
 })
--- gallery/static/scripts/services/mainServices.js
+++ gallery/static/scripts/services/mainServices.js
@@ -1,5 +1,8 @@
 var services = angular.module('ScaffoldsApp.services', []);
 
+
+// queryService factory builds the URI and makes the GET call. Returned data is
+// loaded using the callback() function and then a rootScope broadcast is made
 services.factory('queryService', function($rootScope, $http) {
 
 	var API_URI = 'http://apps.washingtonpost.com/sports/api/nfl/v2/games/?format=jsonp&game_code=';
@@ -12,14 +15,18 @@ services.factory('queryService', function($rootScope, $http) {
 
 		var builtURI = API_URI + code;
 
+		$rootScope.$broadcast('hide');
+
+		// using jsonp to circumvent HTTP Access Control rejections
 		$http.jsonp(builtURI);
 		
+		// functon needs to be defined on the window scope
 		window.callback = function(data) {
-			console.log(data.objects[0]);
 			queryService.returned = data.objects[0];
-			console.log('broadcasting fetched');
 			$rootScope.$broadcast('fetched');
 		}
+
+		$rootScope.$broadcast('show');
 	}
 
 	return queryService;
