db1d695d23efd3493e2286b4b01ca3609e267391
stackBundles <iain.j.mingo@gmail.com>
2013-12-19 10:27:02 -0500

    adding caching to service

--- gallery/static/scripts/services/mainServices.js
+++ gallery/static/scripts/services/mainServices.js
@@ -8,25 +8,39 @@ services.factory('queryService', function($rootScope, $http) {
 	var API_URI = 'http://apps.washingtonpost.com/sports/api/nfl/v2/games/?format=jsonp&game_code=';
 
 	var queryService = {
-		returned: {}
+		returned: {},
+		cached: {}
 	};
 
 	queryService.getGame = function(code) {
 
-		var builtURI = API_URI + code;
-
-		$rootScope.$broadcast('hide');
-
-		// using jsonp to circumvent HTTP Access Control rejections
-		$http.jsonp(builtURI);
-		
-		// functon needs to be defined on the window scope
-		window.callback = function(data) {
-			queryService.returned = data.objects[0];
+		// first check the cache
+		if (queryService.cached[code] != null) {
+			$rootScope.$broadcast('hide');
+			queryService.returned = queryService.cached[code];
 			$rootScope.$broadcast('fetched');
+			$rootScope.$broadcast('show');
+		}
+		// if not in the cache then we fetch the data
+		else {
+			var builtURI = API_URI + code;
+
+			$rootScope.$broadcast('hide');
+
+			// using jsonp to circumvent HTTP Access Control rejections
+			$http.jsonp(builtURI);
+		
+			// functon needs to be defined on the window scope
+			window.callback = function(data) {
+				queryService.returned = data.objects[0];
+				queryService.cached[code] = data.objects[0];
+				$rootScope.$broadcast('fetched');
+			}
+
+			$rootScope.$broadcast('show');
 		}
 
-		$rootScope.$broadcast('show');
+		
 	}
 
 	return queryService;
